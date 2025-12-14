c8946e3d94bc05ca42231b2534554a2853a27d9b
Wilson San <wilson.san57@gmail.com>
2025-12-14 20:30:09 +0100

    creation of workout done without exercices relation

--- src/models/Workout.ts
+++ src/models/Workout.ts
@@ -21,12 +21,12 @@ export class Workout {
 
   @CreateDateColumn() // Date de la séance
   createdAt: Date;
-  
+
   // Relations
-  
-    @Column() 
-    userId: string;
-  
+
+  @Column()
+  userId: string;
+
   @ManyToOne(() => User)
   @JoinColumn({ name: 'userId' })
   user: User;
